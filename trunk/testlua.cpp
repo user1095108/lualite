a03581769d0284aca79e6218a55dae01979ce73c
janezz55@gmail.com <janezz55@gmail.com@2028ac7e-278b-78ef-49e3-65ae9fc3aef2>
2013-03-10 23:30:41 +0000

    properties added

--- trunk/testlua.cpp
+++ trunk/testlua.cpp
@@ -135,6 +135,7 @@ int main(int argc, char* argv[])
     "print(testclass.__classname)\n"
     "print(testclass.smell)\n"
     "local b = testclass.new(1000)\n"
+    "print(\"---\")\n"
     "print(b.a)\n"
     "b:pointer():print(100)\n"
     "b:reference():print_()\n"
