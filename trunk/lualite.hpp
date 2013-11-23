d3928a294d93147cfca4c17d67026d30c3d0849b
janezz55@gmail.com <janezz55@gmail.com@2028ac7e-278b-78ef-49e3-65ae9fc3aef2>
2013-11-23 13:57:40 +0000

    some fixes

--- trunk/lualite.hpp
+++ trunk/lualite.hpp
@@ -1320,7 +1320,7 @@ public:
   scope(char const* const name, A&&... args) :
     name_(name)
   {
-    [](...){ }((args.set_parent_scope(this), 0)...);
+    [](...){}((args.set_parent_scope(this), 0)...);
   }
 
   scope(scope const&) = delete;
@@ -1504,12 +1504,12 @@ protected:
     assert(!lua_gettop(L));
   }
 
-  void set_apply_instance(scope* const instance)
+  void append_child_scope(scope* const instance)
   {
-    auto next(next_);
-
-    if (next)
+    if (next_)
     {
+      auto next(next_);
+
       while (next->next_)
       {
         next = next->next_;
@@ -1527,7 +1527,7 @@ protected:
   {
     parent_scope_ = parent_scope;
 
-    parent_scope->set_apply_instance(this);
+    parent_scope->append_child_scope(this);
   }
 
   void get_scope(lua_State* const L)
@@ -1626,7 +1626,7 @@ public:
     scope(nullptr),
     L_(L)
   {
-    [](...){ }((args.set_parent_scope(this), 0)...);
+    [](...){}((args.set_parent_scope(this), 0)...);
 
     scope::apply(L);
   }
@@ -1636,7 +1636,7 @@ public:
     scope(name),
     L_(L)
   {
-    [](...){ }((args.set_parent_scope(this), 0)...);
+    [](...){}((args.set_parent_scope(this), 0)...);
 
     scope::apply(L);
   }
